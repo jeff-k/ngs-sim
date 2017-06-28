90b3b5cf873347daa36098b69c89c75457438c7c
Jeff Knaggs <jknaggs@cfenet.ubc.ca>
2017-06-28 15:49:37 -0700

    hardcoded paths; very alpha

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # ngs-sim
-Simulate next-generation sequencing artefacts
+Synthetic next-generation sequencing data with artefacts
+
+## Mixtures
+
+Multiple fasta references and mutation profiles can be provided. A proportion argument specifies the concentration of each. If no proportion is specified the default is 1.
+
+## Mutations
+
+Mutations are defined in an ad hoc "variant call format" file. This is a list of:
+
+```
+position	deletions	insertions
+```
+
+Where `position` is the 0 indexed position in the reference sequence, `deletions` is the number of bases to remove, and `insertions` is the string of bases to insert.
+
+## Simulating sequencing artefacts
+
+Optionally, the sequencing error profile is defined as a transition matrix.
+
+## Simulation
+
+The simulation chains the following passes:
+
+* mutation: specified variants are spiked into the reference
+* amplification: the sequencing error profile is applied
+* fragmentation: mutated strands are truncated to the mean and std fragment size. Pairs of mated reads are read from the ends. The second mate is reverse complemented.
---
+++ spikein.py
@@ -0,0 +1,133 @@
+#!/usr/bin/env python3
+"""
+In-silico NGS data synthesis
+"""
+
+import itertools
+import argparse
+import numpy as np
+
+def write_fastq(seq, rname):
+    """Return a 4 line string representing a fastq record"""
+    qual = []
+    for _ in range(len(seq)):
+        #qual.append(chr(int(np.floor(np.random.normal(70, 5)))))
+        qual.append('H')
+    return "{}\n{}\n{}\n{}".format(rname, seq, '+', ''.join(qual))
+
+def read_fasta(f):
+    """Parse a single reference fasta file into a string"""
+    lines = []
+    for line in open(f):
+        if line[0] == '>':
+            continue
+        lines.append(line.strip())
+    return ''.join(lines)
+
+def rc(seq):
+    # complementary ambiguous nucleotide codes
+    d = {'A': 'T', 'C': 'G', 'G': 'C', 'T': 'A',
+            'R': 'Y', 'Y': 'R', 'S': 'S', 'W': 'W',
+            'K': 'M', 'M': 'K', 'B': 'V', 'V': 'B',
+            'D': 'H', 'H': 'D'}
+    return ''.join((map(lambda x: d[x], reversed(seq))))
+
+def mutate(ops, seq):
+    """Apply a list of operations to a reference sequence"""
+    
+    for p, d, i in reversed(sorted(ops, key=lambda x: x[2])):
+        if i == '.':
+            seq = seq[:p] + seq[p+d:]
+        else:
+            seq = seq[:p] + i + seq[p+d:]
+    return seq
+
+class Op:
+    def __init__(self, pos, d, ins):
+        """Initialize an operation
+        
+        pos: the reference coordinate of the mutation
+        d: the number of deleted bases
+        ins: the string of inserted bases
+        """
+        self.d = d
+        self.ins = ins
+        self.pos = pos
+
+    def __call__(self, template):
+        return template[:self.pos] + self.ins + template[self.pos + self.d:]
+
+class ChemProfile:
+    def __init__(self, frag_mean, read_size, adapters=None, sigma=100,
+                 error_profile=False):
+        self.frag_mean = frag_mean
+        self.frag_std = sigma
+        self.read_size = read_size
+        if error_profile:
+            self.err_mat = np.array([
+                    [0.97, 0.01, 0.01, 0.01],
+                    [0.01, 0.97, 0.01, 0.01],
+                    [0.01, 0.01, 0.97, 0.01],
+                    [0.01, 0.01, 0.01, 0.97]
+                ])
+        else:
+            self.err_mat = None
+
+    def fragment(self, seq):
+        s = int(np.floor(np.random.normal(self.frag_mean, self.frag_std)))
+        p = int(np.floor(np.random.randint(0, len(seq) - s)))
+        frag = seq[p:p+s]
+        return frag[:self.read_size], rc(frag[-self.read_size:])
+
+    def amplify(self, seq):
+        seq = list(seq)
+        for i in range(len(seq)):
+            base = seq[i]
+            ps = self.err_mat[['A','C','G','T'].index(base)]
+            seq[i] = np.random.choice(['A','C','G','T'], p=ps)
+        return ''.join(seq)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('ref')
+    parser.add_argument('-p', default=1)
+    parser.add_argument('-n', '--num', default=10000)
+    parser.add_argument('--mutations', '-m', default=[])
+    parser.add_argument('--template', '-t', default=None)
+    args = parser.parse_args()
+
+    prof = ChemProfile(2000, 151)
+    reads = []
+    fq1 = open("fq1.fastq", 'w')
+    fq2 = open("fq2.fastq", 'w')
+   
+    #mutations = parse_mutations(args.mutations)
+    mutations = [("empty.vcf", 1), ("mutations.vcf", 9),]
+
+    y = 0
+    total_p = sum(map(lambda x: x[1], mutations))
+    conseq_p = max(map(lambda x: x[1], mutations))
+    total_reads = args.num
+    conseq = None
+    for m, f in mutations:
+        y += 1
+        ops = []
+        seq = read_fasta(args.ref).upper()
+        for line in open(m):
+            if line[0] == '#':
+                continue
+            p, d, i = line.strip().split()
+            ops.append((int(p),int(d),i.upper()))
+        proportion = f / total_p
+
+        sub_seq = mutate(ops, seq)
+        if f == conseq_p:
+            conseq = str(seq)
+
+        for count in range(int(total_reads * proportion)):
+            r1, r2 = prof.fragment(prof.amplify(sub_seq))
+            print(write_fastq(r1, "@SYNTH:0:0000000:0:0:{}:{} 1:N:0:0".format(
+                y, count)), file=fq1)
+            print(write_fastq(r2, "@SYNTH:0:0000000:0:0:{}:{} 2:N:0:0".format(
+                y, count)), file=fq2)
+    print(conseq, file=open("conseq.fasta", 'w'))
